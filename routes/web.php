daab0e07fd9b32823920e660de9abaf2f56bbfb4
Junior <duartejusanca@gmail.com>
2024-04-29 12:21:34 +0200

    Autoloading, Namespace and Models

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,19 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/jobs', function () {
+Route::get('/jobs', function (){
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Community',
-                'salary' => "30k"
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => "50k"
-            ],
-            [
-                'id' => 3,
-                'title' => 'YouTuber',
-                'salary' => "100k"
-            ],
-        ]
+        'jobs' => Job::all()
     ]);
 });
 Route::get('/jobs/{id}', function ($id) {
-        $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Community',
-                'salary' => "30k"
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => "50k"
-            ],
-            [
-                'id' => 3,
-                'title' => 'YouTuber',
-                'salary' => "100k"
-            ],
-        ];
-
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-        // dd($job);
-        return view('job', ['job' => $job]);
+    $job = Job::find($id);
+    return view('job', ['job' => $job]);
 });
 Route::get('/contact', function () {
     return view('contact');
