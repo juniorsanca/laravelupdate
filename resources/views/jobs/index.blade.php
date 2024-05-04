3ff3d8977e3c89b44cafe973aabcaaafb3533ed5
Junior <duartejusanca@gmail.com>
2024-05-04 22:54:42 +0200

    create new view and route to our create job form with type get and post

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -5,7 +5,7 @@
     <div class="space-y-4">
         @foreach ($jobs as $job )
             <div>
-                <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200">
+                <a href="/jobs/show/{{$job['id']}}" class="block px-4 py-6 border border-gray-200">
                 <p class="">{{$job->employer['name']}}</p>
                 <strong>Poste</strong> : {{$job['title']}} - <strong>Salaire</strong> : {{$job['salary']}}</a>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,24 @@ Route::get('/jobs', function () {
 
     $jobs = Job::with('employer')->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('job', ['job' => $job]);
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
+
+Route::post('/jobs', function () {
+    dd('hello post');
+});
+
+Route::get('/jobs/show/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.show', ['job' => $job]);
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
