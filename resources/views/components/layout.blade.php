fc911d909fbb89fb96bba8c0df15554603dd4c1b
Junior <duartejusanca@gmail.com>
2024-05-11 02:42:20 +0200

    vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        @vite(['resources/css/app.css'])
         <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Fonts -->
